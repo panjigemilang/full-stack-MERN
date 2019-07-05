e2cccf86187d1dd37d8cedcdd93e6e0e70bf8e33
Omnious <panjigemilang31298@gmail.com>
2019-07-05 09:44:44 +0700

    prepared to deploy final

--- config/keys.js
+++ config/keys.js
@@ -1,25 +1,3 @@
-// if (process.env.NODE_ENV === 'production') {
-//     module.exports = require('./keys_prod');
-// } else {
-//     module.exports = require('./keys_dev');
-// }
-
-// module.exports = require('./keys_dev');
-
-// const mongo = require('mongoose');
-// const uname = "panjigemilang";
-// const upass = "xtremesuper98";
-
-// module.exports = {
-//     mongoURI: mongo.connect("mongodb+srv://${user}:${pass}~@devconnector-vasnt.mongodb.net/test?retryWrites=true", {
-//         uri_decode_auth: true
-//     }, function (err, db) {
-
-//     })
-// }
-
-// const mongoURI = "mongodb+srv://panjigemilang:xtremesuper98@tmcluster-li9pg.mongodb.net/test?retryWrites=true";
-
 if (process.env.NODE_ENV === "production") {
   module.exports = require("./keys_prod")
 } else {
--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,19 +1,5 @@
-// const mongoClient = require('mongoose');
-
-// mongoClient.connect("mongodb+srv://panjigemilang31298%40gmail%2Ecom:xtremesuper98~@devconnector-vasnt.mongodb.net/test?retryWrites=true", {
-//     uri_decode_auth: true
-// }, function (err, db) {
-
-// });
-
-// module.exports = {
-//   mongoURI: `mongodb+srv://${encodeURIComponent(
-//     "panjigemilang31298%40gmail.com"
-//   )}:xtremesuper98~@devconnector-vasnt.mongodb.net/test?retryWrites=true`
-// }
-
 module.exports = {
   mongoURI:
     "mongodb+srv://panjigemilang:xtremesuper98@devconnector-vasnt.mongodb.net/test?retryWrites=true&w=majority",
-  secretorKey: "secret"
+  secretOrKey: "secret"
 }
